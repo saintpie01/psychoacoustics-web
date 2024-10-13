a24eb535e8c42b20637bf163ca288ef3e321eae5
saintpie01 <santolinpietro@gmail.com>
2024-10-13 22:41:27 +0200

    comments

--- yourTests.php
+++ yourTests.php
@@ -1,11 +1,12 @@
 <?php
-    session_start();
-    //check if there is a user logged
-    if (!isset($_SESSION['currentLoggedUsername']) || !isset($_SESSION['currentLoggedID']))
-        header("Location: index.php");
-    include "php/config.php";
+session_start();
+
+//check if there is a user logged
+if (!isset($_SESSION['currentLoggedUsername']) || !isset($_SESSION['currentLoggedID']))
+    header("Location: index.php");
+include "php/config.php";
 ?>
-    
+
 <!doctype html>
 <html lang="en">
 
@@ -24,132 +25,142 @@
 
 
 <body>
-<nav class="navbar navbar-dark bg-dark shadow-lg text-white">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="files/logo.png" alt="" width="25" height="25" class="d-inline-block align-text-top">
-            PSYCHOACOUSTICS-WEB
-        </a>
-        <form class="d-flex align-items-center">
-            <label class='text-white navbar-text me-3'>Welcome <?php echo $_SESSION['currentLoggedUsername']; ?></label>
-            <button class="btn btn-outline-light me-3" type="button" onclick="location.href='yourTests.php'">
-                Your tests
-            </button>
-            <button class="btn btn-outline-light me-3" type="button" onclick="location.href='php/logout.php'">
-                Log Out
-            </button>
-            <a class='settings navbar-text' href='userSettings.php'>
-                <i class='material-icons rotate text-white'>settings</i>
+    <nav class="navbar navbar-dark bg-dark shadow-lg text-white">
+        <div class="container">
+
+            <!-- Site title -->
+            <a class="navbar-brand" href="index.php">
+                <img src="files/logo.png" alt="" width="25" height="25" class="d-inline-block align-text-top">
+                PSYCHOACOUSTICS-WEB
             </a>
-        </form>
-    </div>
-</nav>
 
-<div class="container">
-    <h1 class="mt-5">Welcome <?php echo $_SESSION['currentLoggedUsername'];?> <?php echo '  #'. $_SESSION['currentLoggedID'];?> </h1>
-    <div class="row g-3">
-        <div class="col d-grid">
-            <button type='button' class='btn btn-primary btn-lg btn-red'
+            <!-- user Login and Settings Buttons -->
+            <form class="d-flex align-items-center">
+                <label class='text-white navbar-text me-3'>Welcome <?php echo $_SESSION['currentLoggedUsername']; ?></label>
+                <button class="btn btn-outline-light me-3" type="button" onclick="location.href='yourTests.php'">
+                    Your tests
+                </button>
+                <button class="btn btn-outline-light me-3" type="button" onclick="location.href='php/logout.php'">
+                    Log Out
+                </button>
+                <a class='settings navbar-text' href='userSettings.php'>
+                    <i class='material-icons rotate text-white'>settings</i>
+                </a>
+            </form>
+
+        </div>
+    </nav>
+
+
+    <div class="container">
+        <h1 class="mt-5">Welcome <?php echo $_SESSION['currentLoggedUsername']; ?> <?php echo '  #' . $_SESSION['currentLoggedID']; ?> </h1>
+        <div class="row g-3">
+
+            <!-- download all your data Button -->
+            <div class="col d-grid">
+                <button type='button' class='btn btn-primary btn-lg btn-red'
                     onclick='location.href="php/downloadYours.php?all=1"'>
-                Download all your data
-            </button>
-        </div>
-        <div class="col d-grid">
-            <button type='button' class='btn btn-primary btn-lg btn-red'
+                    Download all your data
+                </button>
+            </div>
+
+            <!-- Download all your guest's data Button -->
+            <div class="col d-grid">
+                <button type='button' class='btn btn-primary btn-lg btn-red'
                     onclick='location.href="php/downloadYours.php?all=0"'>
-                Download all your guest's data
-            </button>
-        </div>
-        <?php
-        try {
-            $conn = new mysqli($host, $user, $password, $dbname);
+                    Download all your guest's data
+                </button>
+            </div>
+            
+            <?php
+            try {
+                $conn = new mysqli($host, $user, $password, $dbname);
 
-            if ($conn->connect_errno)
-                throw new Exception('DB connection failed');
+                if ($conn->connect_errno)
+                    throw new Exception('DB connection failed');
 
-            mysqli_set_charset($conn, "utf8");
+                mysqli_set_charset($conn, "utf8");
 
-            $usr = $_SESSION['currentLoggedUsername'];
-            $id = $_SESSION['currentLoggedID'];
+                $usr = $_SESSION['currentLoggedUsername'];
+                $id = $_SESSION['currentLoggedID'];
 
-            $sql = "SELECT Type FROM account WHERE Guest_ID='$id' AND Username='$usr'";
-            $result = $conn->query($sql);
-            $row = $result->fetch_assoc();
-            if ($row['Type'] == 1) { ?>
-                <div class="col d-grid">
-                    <button type='button' class='btn btn-primary btn-lg btn-red'
+                $sql = "SELECT Type FROM account WHERE Guest_ID='$id' AND Username='$usr'";
+                $result = $conn->query($sql);
+                $row = $result->fetch_assoc();
+                if ($row['Type'] == 1) { ?>
+                    <div class="col d-grid">
+                        <button type='button' class='btn btn-primary btn-lg btn-red'
                             onclick='location.href="php/downloadAll.php"'>
-                        Download all the data in the database
-                    </button>
-                </div>
+                            Download all the data in the database
+                        </button>
+                    </div>
             <?php }
-        } catch (Exception $e) {
-            header("Location: index.php?err=db");
-        }
-        ?>
-    </div>
+            } catch (Exception $e) {
+                header("Location: index.php?err=db");
+            }
+            ?>
+        </div>
 
-    <h3 class="mt-5">Your results</h3>
-    <table class="table table-striped table-hover">
-        <thead>
-        <tr>
-            <th scope="col">Test</th>
-            <th scope="col">Time</th>
-            <th scope="col">Type</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        try {
-            $sql = "SELECT Test_count, Timestamp, Type FROM test WHERE Guest_ID='$id'";
-            $result = $conn->query($sql);
-            while ($row = $result->fetch_assoc()) { ?>
+        <h3 class="mt-5">Your results</h3>
+        <table class="table table-striped table-hover">
+            <thead>
                 <tr>
-                    <td><?php echo $row["Test_count"]; ?></td>
-                    <td><?php echo $row["Timestamp"]; ?></td>
-                    <td><?php echo $row["Type"]; ?></td>
+                    <th scope="col">Test</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Type</th>
                 </tr>
-            <?php }
-        } catch (Exception $e) {
-            header("Location: index.php?err=db");
-        }
-        ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                try {
+                    $sql = "SELECT Test_count, Timestamp, Type FROM test WHERE Guest_ID='$id'";
+                    $result = $conn->query($sql);
+                    while ($row = $result->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?php echo $row["Test_count"]; ?></td>
+                            <td><?php echo $row["Timestamp"]; ?></td>
+                            <td><?php echo $row["Type"]; ?></td>
+                        </tr>
+                <?php }
+                } catch (Exception $e) {
+                    header("Location: index.php?err=db");
+                }
+                ?>
+            </tbody>
+        </table>
 
-    <h3 class="mt-5">Your guest's results</h3>
-    <table class="table table-striped table-hover">
-        <thead>
-        <tr>
-            <th scope="col">Test</th>
-            <th scope="col">Time</th>
-            <th scope="col">Type</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        try {
-            $sql = "SELECT Name, Test_count, Timestamp, Type FROM test INNER JOIN guest ON Guest_ID=ID WHERE fk_guest='{$_SESSION['currentLoggedUsername']}'";
-            $result = $conn->query($sql);
-            while ($row = $result->fetch_assoc()) { ?>
+        <h3 class="mt-5">Your guest's results</h3>
+        <table class="table table-striped table-hover">
+            <thead>
                 <tr>
-                    <td><?php echo $row["Name"]; ?></td>
-                    <td><?php echo $row["Test_count"]; ?></td>
-                    <td><?php echo $row["Timestamp"]; ?></td>
-                    <td><?php echo $row["Type"]; ?></td>
+                    <th scope="col">Test</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Type</th>
                 </tr>
-            <?php }
-        } catch (Exception $e) {
-            header("Location: index.php?err=db");
-        }
-        ?>
-    </table>
-    <div class="d-grid my-5">
-        <button type="button" class="btn btn-primary btn-lg btn-red" id="home" onclick="location.href='index.php'">Home</button>
+            </thead>
+            <tbody>
+                <?php
+                try {
+                    $sql = "SELECT Name, Test_count, Timestamp, Type FROM test INNER JOIN guest ON Guest_ID=ID WHERE fk_guest='{$_SESSION['currentLoggedUsername']}'";
+                    $result = $conn->query($sql);
+                    while ($row = $result->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?php echo $row["Name"]; ?></td>
+                            <td><?php echo $row["Test_count"]; ?></td>
+                            <td><?php echo $row["Timestamp"]; ?></td>
+                            <td><?php echo $row["Type"]; ?></td>
+                        </tr>
+                <?php }
+                } catch (Exception $e) {
+                    header("Location: index.php?err=db");
+                }
+                ?>
+        </table>
+        <div class="d-grid my-5">
+            <button type="button" class="btn btn-primary btn-lg btn-red" id="home" onclick="location.href='index.php'">Home</button>
+        </div>
     </div>
-</div>
 
 </body>
+
 </html>
-
-
