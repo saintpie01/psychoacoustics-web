00bd536c7042a14ec170c51e0e25392882f928ea
saintpie01 <santolinpietro@gmail.com>
2024-11-27 21:49:00 +0100

    moved php logic on top

--- yourTests.php
+++ yourTests.php
@@ -8,6 +8,33 @@ include_once "php/helpers/utils.php";
 if (!isUserLogged())
     header("Location: index.php");
 
+$conn = connectdb();
+
+$usr = $_SESSION['loggedUser']['username'];
+$id = $_SESSION['loggedUser']['id'];
+
+try {
+
+    // Get all your tests
+    $sql = "SELECT Guest_ID, Test_count, Timestamp, Type FROM test WHERE Guest_ID='$id' AND ref_name = ''";
+    $yourResult = $conn->query($sql);
+
+    // Get all guest tests
+    $sql = "SELECT ID, Name, Test_count, Timestamp, Type FROM test INNER JOIN guest ON Guest_ID=ID WHERE fk_guest='{$_SESSION['loggedUser']['username']}'";
+    $guestResult = $conn->query($sql);
+
+    // Get user Type
+    $sql = "SELECT Type FROM account WHERE Guest_ID='$id' AND Username='$usr'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $checkSuperuser = $row['Type'];
+
+
+} catch (Exception $e) {
+    header("Location: index.php?err=db");
+}
+
+
 ?>
 
 
@@ -37,48 +64,37 @@ if (!isUserLogged())
 
 
     <div class="container">
-        <h1 class="mt-5 pt-5">Welcome <?php echo $_SESSION['currentLoggedUsername']; ?> <?php echo '  #' . $_SESSION['currentLoggedID']; ?> </h1>
+        <h1 class="mt-5 pt-5">Welcome <?php echo $_SESSION['loggedUser']['username']; ?> <?php echo '  #' . $_SESSION['loggedUser']['id']; ?> </h1>
         <div class="row g-3">
 
             <!-- download all your data Button -->
             <div class="col d-grid">
                 <button type='button' class='btn btn-primary btn-lg btn-red'
-                    onclick='location.href="php/download_yours.php?all=1"'>
-                    Download all data
+                    onclick='location.href="php/download_tests.php?type=0"'>
+                    Download all your personal data
                 </button>
             </div>
 
             <!-- Download all your guest's data Button -->
             <div class="col d-grid">
                 <button type='button' class='btn btn-primary btn-lg btn-red'
-                    onclick='location.href="php/download_yours.php?all=0"'>
+                    onclick='location.href="php/download_tests.php?type=1"'>
                     Download all your partecipants' data
                 </button>
             </div>
 
 
             <?php
+
             //function deicated to Superuser
-            try {
-                $conn = connectdb();
-
-                $usr = $_SESSION['currentLoggedUsername'];
-                $id = $_SESSION['currentLoggedID'];
-
-                $sql = "SELECT Type FROM account WHERE Guest_ID='$id' AND Username='$usr'";
-                $result = $conn->query($sql);
-                $row = $result->fetch_assoc();
-                if ($row['Type'] == 1) { ?>
-                    <div class="col d-grid">
-                        <button type='button' class='btn btn-primary btn-lg btn-red'
-                            onclick='location.href="php/download_all.php"'>
-                            Download all the data in the database
-                        </button>
-                    </div>
+            if ($checkSuperuser == 1) { ?>
+                <div class="col d-grid">
+                    <button type='button' class='btn btn-primary btn-lg btn-red'
+                        onclick='location.href="php/download_tests.php?type=2"'>
+                        Download all the data in the database
+                    </button>
+                </div>
             <?php }
-            } catch (Exception $e) {
-                header("Location: index.php?err=db");
-            }
             ?>
 
         </div>
@@ -96,9 +112,8 @@ if (!isUserLogged())
             <tbody>
                 <?php
                 try {
-                    $sql = "SELECT Guest_ID, Test_count, Timestamp, Type FROM test WHERE Guest_ID='$id' AND ref_name = ''";
-                    $result = $conn->query($sql);
-                    while ($row = $result->fetch_assoc()) { ?>
+
+                    while ($row = $yourResult->fetch_assoc()) { ?>
                         <tr>
                             <td><?php echo $row["Test_count"]; ?></td>
                             <td><?php echo $row["Timestamp"]; ?></td>
@@ -130,7 +145,7 @@ if (!isUserLogged())
 
 
         <!-- Guest's results Table -->
-        <h3 class="mt-5">Partecipants' results</h3>
+        <h3 class="mt-5">Partecipants' results (<?php echo $guestResult->num_rows; ?>)</h3>
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
@@ -142,10 +157,9 @@ if (!isUserLogged())
             <tbody>
                 <?php
                 try {
-                    $sql = "SELECT ID, Name, Test_count, Timestamp, Type FROM test INNER JOIN guest ON Guest_ID=ID WHERE fk_guest='{$_SESSION['currentLoggedUsername']}'";
-                    $result = $conn->query($sql);
 
-                    while ($row = $result->fetch_assoc()) { ?>
+
+                    while ($row = $guestResult->fetch_assoc()) { ?>
                         <tr>
                             <td><?php echo $row["Name"]; ?></td>
                             <td><?php echo $row["Timestamp"]; ?></td>
