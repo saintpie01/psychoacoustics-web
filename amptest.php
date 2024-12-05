8cc86361d6db2c49668866d4c3a1c48f6c13a269
saintpie01 <santolinpietro@gmail.com>
2024-12-05 03:33:28 +0100

    exported variable ini. in different file

--- amptest.php
+++ amptest.php
@@ -11,35 +11,13 @@ $testMsg = "Which is the loudest tone?";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="files/logo.png">
 
+    <title>Psychoacoustics-web - Amplitude test</title>
+
     <!-- Bootstrap CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Psychoacoustics-web - Amplitude test</title>
+    <?php include 'view_modules/export_test_parameters.php'; ?>
 
-    <script type="text/javascript" src="js/fetchTexts.js"></script>
-
-    <script>
-        // pass info from php session to js
-        var amp = parseFloat(<?php echo $_SESSION["amplitude"]; ?>);
-        var freq = parseFloat(<?php echo $_SESSION["frequency"]; ?>);
-        var dur = parseFloat(<?php echo $_SESSION["duration"]; ?>);
-        var onRamp = parseFloat(<?php echo $_SESSION["onRamp"]; ?>);
-        var offRamp = parseFloat(<?php echo $_SESSION["offRamp"]; ?>);
-        var blocks = parseInt(<?php echo $_SESSION["blocks"]; ?>);
-        var delta = parseFloat(<?php echo $_SESSION["delta"]; ?>);
-        var nAFC = parseInt(<?php echo $_SESSION["nAFC"]; ?>);
-        var ITI = parseInt(<?php echo $_SESSION["ITI"]; ?>);
-        var ISI = parseInt(<?php echo $_SESSION["ISI"]; ?>);
-        var feedback = parseInt(<?php echo $_SESSION["checkFb"]; ?>);
-        var factor = parseFloat(<?php echo $_SESSION["factor"]; ?>);
-        var secondFactor = parseFloat(<?php echo $_SESSION["secFactor"]; ?>);
-        var reversals = parseInt(<?php echo $_SESSION["reversals"]; ?>);
-        var secondReversals = parseInt(<?php echo $_SESSION["secReversals"]; ?>);
-        var reversalThreshold = parseInt(<?php echo $_SESSION["threshold"]; ?>);
-        var algorithm = <?php echo "'{$_SESSION["algorithm"]}'"; ?>;
-        var currentBlock = parseInt(<?php if (isset($_SESSION["currentBlock"])) echo $_SESSION["currentBlock"] + 1;
-                                    else echo "1" ?>);
-    </script>
     <script type="text/javascript"
         src="js/test_common/generatorSoundAndNoise.js<?php if (isset($_SESSION['version'])) echo "?{$_SESSION['version']}"; ?>"
         defer></script>
@@ -48,11 +26,11 @@ $testMsg = "Which is the loudest tone?";
         src="js/soundsAmplitude.js<?php if (isset($_SESSION['version'])) echo "?{$_SESSION['version']}"; ?>"
         defer></script>
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </head>
 
 <body>
-    <?php include 'html_modules/test_dashboard.php'; ?>
-
+    <?php include 'view_modules/test_dashboard.php'; ?>
 </body>
 
 </html>
--- durtest.php
+++ durtest.php
@@ -21,28 +21,7 @@ $testMsg = "Which is the longest tone?";
 
     <script type="text/javascript" src="js/fetchTexts.js"></script>
 
-    <script>
-        var amp = parseFloat(<?php echo $_SESSION["amplitude"]; ?>);
-        var freq = parseFloat(<?php echo $_SESSION["frequency"]; ?>);
-        var dur = parseFloat(<?php echo $_SESSION["duration"]; ?>);
-        var onRamp = parseFloat(<?php echo $_SESSION["onRamp"]; ?>);
-        var offRamp = parseFloat(<?php echo $_SESSION["offRamp"]; ?>);
-        //var phase = <//?php echo $_SESSION["phase"]; ?>;
-        var blocks = parseInt(<?php echo $_SESSION["blocks"]; ?>);
-        var delta = parseFloat(<?php echo $_SESSION["delta"]; ?>);
-        var nAFC = parseInt(<?php echo $_SESSION["nAFC"]; ?>);
-        var ITI = parseInt(<?php echo $_SESSION["ITI"]; ?>);
-        var ISI = parseInt(<?php echo $_SESSION["ISI"]; ?>);
-        var feedback = parseInt(<?php echo $_SESSION["checkFb"]; ?>);
-        var factor = parseFloat(<?php echo $_SESSION["factor"]; ?>);
-        var secondFactor = parseFloat(<?php echo $_SESSION["secFactor"]; ?>);
-        var reversals = parseInt(<?php echo $_SESSION["reversals"]; ?>);
-        var secondReversals = parseInt(<?php echo $_SESSION["secReversals"]; ?>);
-        var reversalThreshold = parseInt(<?php echo $_SESSION["threshold"]; ?>);
-        var algorithm = <?php echo "'{$_SESSION["algorithm"]}'"; ?>;
-        var currentBlock = parseInt(<?php if (isset($_SESSION["currentBlock"])) echo $_SESSION["currentBlock"] + 1;
-                                    else echo "1" ?>);
-    </script>
+    <?php include 'view_modules/export_test_parameters.php'; ?>
     <script type="text/javascript"
         src="js/test_common/generatorSoundAndNoise.js<?php if (isset($_SESSION['version'])) echo "?{$_SESSION['version']}"; ?>"
         defer></script>
@@ -54,7 +33,7 @@ $testMsg = "Which is the longest tone?";
 </head>
 
 <body>
-    <?php include 'html_modules/test_dashboard.php'; ?>
+    <?php include 'view_modules/test_dashboard.php'; ?>
 </body>
 
 </html>
